2df576046137962539bcb258c8d9fb24372aeb79
pragatisharma23 <pragati@somish.com>
2020-03-24 13:25:51 +0530

    add test cases for all users

--- test/01_Market.js
+++ test/01_Market.js
@@ -48,22 +48,71 @@ contract('Market', function([
       await marketInstance.setPrice(1,10);
       const getPrice = await marketInstance.getPrice(1);
       assert.equal(getPrice/1,10);
-      // await marketInstance.setPrice(1,20);
-      // const getPrice2 = await marketInstance.getPrice(1);
-      // assert.equal(getPrice2/1,20);
-      
-    })
+      await marketInstance.setPrice(2,20);
+      const getPrice1 = await marketInstance.getPrice(2);
+      assert.equal(getPrice1/1,20);
+      await marketInstance.setPrice(3,30);
+      const getPrice2 = await marketInstance.getPrice(3);
+      assert.equal(getPrice2/1,30);
+      await marketInstance.setPrice(4,40);
+      const getPrice3 = await marketInstance.getPrice(4);
+      assert.equal(getPrice3/1,40);
+      await marketInstance.setPrice(1,50);
+      const getPrice4 = await marketInstance.getPrice(1);
+      assert.equal(getPrice4/1,50);
+      await marketInstance.setPrice(2,20);
+      const getPrice5 = await marketInstance.getPrice(2);
+      assert.equal(getPrice5/1,20);
+      await marketInstance.setPrice(5,30);
+      const getPrice6 = await marketInstance.getPrice(5);
+      assert.equal(getPrice6/1,30);
+      await marketInstance.setPrice(6,20);
+      const getPrice7 = await marketInstance.getPrice(6);
+      assert.equal(getPrice7/1,20);
+      await marketInstance.setPrice(7,50);
+      const getPrice8= await marketInstance.getPrice(7);
+      assert.equal(getPrice8/1,50);
+      })
 
     it('user bet points',async function() {
       // await marketInstance.setPrice(7,10);
       // const getPrice = await marketInstance.getPrice(7);
-      await marketInstance.placeBet(1,{value: 1,from: user1});
-      const getbrttingpoint = await marketInstance.userBettingPoints(user1,1);
-      assert.equal(getbrttingpoint/1,100);
-      // await marketInstance.placeBet(1,{value: 1,from: user5});
-      // const getbrttingpoint1 = await marketInstance.userBettingPoints(user5,1);
-      // console.log(getbrttingpoint1/1)
-      // assert.equal(getbrttingpoint1/1,50000000000000000);
+      await marketInstance.placeBet(1,{value: 4,from: user1});
+      await marketInstance.placeBet(2,{value: 6,from: user2});
+      await marketInstance.placeBet(3,{value: 2,from: user3});
+      await marketInstance.placeBet(4,{value: 4,from: user4});
+      await marketInstance.placeBet(1,{value: 3,from: user5});
+      await marketInstance.placeBet(2,{value: 2,from: user6});
+      await marketInstance.placeBet(5,{value: 5,from: user7});
+      await marketInstance.placeBet(6,{value: 5,from: user8});
+      await marketInstance.placeBet(7,{value: 7,from: user9});
+      const getbrttingpoint  = await marketInstance.userBettingPoints(user1,1);
+      const getbrttingpoint1 = await marketInstance.userBettingPoints(user2,2);
+      const getbrttingpoint2 = await marketInstance.userBettingPoints(user3,3);
+      const getbrttingpoint3 = await marketInstance.userBettingPoints(user4,4);
+      const getbrttingpoint4 = await marketInstance.userBettingPoints(user5,1);
+      const getbrttingpoint5 = await marketInstance.userBettingPoints(user6,2);
+      const getbrttingpoint6 = await marketInstance.userBettingPoints(user7,5);
+      const getbrttingpoint7 = await marketInstance.userBettingPoints(user8,6);
+      const getbrttingpoint8 = await marketInstance.userBettingPoints(user9,7);
+      console.log("user1",getbrttingpoint/1)
+      assert.equal(getbrttingpoint/1,400);
+      console.log("user2",getbrttingpoint1/1)
+      assert.equal(getbrttingpoint1/1,300);
+      console.log("user3",getbrttingpoint2/1)
+      assert.equal(getbrttingpoint2/1,66.66);
+      console.log("user4",getbrttingpoint3/1)
+      assert.equal(getbrttingpoint3/1,100);
+      console.log("user5",getbrttingpoint4/1)
+      assert.equal(getbrttingpoint4/1,60);
+      console.log("user6",getbrttingpoint5/1)
+      assert.equal(getbrttingpoint5/1,100);
+      console.log("user7",getbrttingpoint6/1)
+      assert.equal(getbrttingpoint6/1,166.666);
+      console.log("user8",getbrttingpoint7/1)
+      assert.equal(getbrttingpoint7/1,250);
+      console.log("user9",getbrttingpoint8/1)
+      assert.equal(getbrttingpoint8/1,140);
     }) 
      
 })
