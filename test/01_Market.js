ba19ae6e8d86c979c566321224e3c0e5295c8289
pragatisharma23 <pragati@somish.com>
2020-03-25 18:55:34 +0530

    option values and reward distribution test case added

--- test/01_Market.js
+++ test/01_Market.js
@@ -30,20 +30,49 @@ contract('Market', function([
   operator
 ]) {
   let MarketInstance 
-    let nowTime = new Date()/1000;
-    nowTime = parseInt(nowTime);
-    let thenTime = new BN(((nowTime/1+(60)).toString()));
-    const uintparams = [nowTime,thenTime,1,2,1,7,1000000000000000,10000,2,2,100]
-    const feedsource  = "jonas"
-    const stockName =   "0x47"
-    const addressParams = ["0x81A69EE30637601356ff15d6c4a905079b53FCE1","0x81A69EE30637601356ff15d6c4a905079b53FCE1"]
+    // let nowTime = new Date()/1000;
+    // nowTime = parseInt(nowTime);
+    // let thenTime = new BN(((nowTime/1+(300)).toString()));
+    // let thenTime = new BN(((1585119864/1+(600)).toString()));
+    // const uintparams = [1585121085,1585121385,1,2,1,7,1000000000000000,10000,2,2,100]
+    // const feedsource  = "jonas"
+    // const stockName =   "0x47"
+    // const addressParams = ["0x81A69EE30637601356ff15d6c4a905079b53FCE1","0x81A69EE30637601356ff15d6c4a905079b53FCE1"]
     beforeEach(async () => {
         marketInstance = await Market.deployed()
-        // console.log(marketInstance.address)
         assert.ok(marketInstance)
     })
+    it('set option price',async function(){
+          const options1 = await marketInstance.optionsAvailable(1);
 
-    it('set price of option',async function() {
+          assert.equal(options1[0]/1,0)
+          assert.equal(options1[1]/1,9749)
+          const options2 = await marketInstance.optionsAvailable(2);
+          assert.equal(options2[0]/1,9750)
+          assert.equal(options2[1]/1,9849)
+          const options3 = await marketInstance.optionsAvailable(3);
+          assert.equal(options3[0]/1,9850)
+          assert.equal(options3[1]/1,9949)
+          const options4 = await marketInstance.optionsAvailable(4);
+          assert.equal(options4[0]/1,9950)
+          assert.equal(options4[1]/1,10050)
+          const options5 = await marketInstance.optionsAvailable(5);
+          assert.equal(options5[0]/1,10051)
+          assert.equal(options5[1]/1,10150)
+          const options6 = await marketInstance.optionsAvailable(6);
+          assert.equal(options6[0]/1,10151)
+          assert.equal(options6[1]/1,10250)
+          const options7 = await marketInstance.optionsAvailable(7);
+          assert.equal(options7[0]/1,10251)
+          assert.equal(options7[1]/1,999999999999)
+          // const options = await marketInstance.optionsAvailable(7);
+          // assert.equal(options[0]/1,10251)
+          // assert.equal(options[1]/1,999999999999) const options = await marketInstance.optionsAvailable(7);
+          // assert.equal(options[0]/1,10251)
+          // assert.equal(options[1]/1,9999999999
+      
+    })
+    it('place bet from nine users',async function() {
 
       await marketInstance.setPrice(1,10);
       const getPrice = await marketInstance.getPrice(1);
@@ -97,25 +126,41 @@ contract('Market', function([
       const getPrice8= await marketInstance.getPrice(7);
       assert.equal(getPrice8/1,50);
       await marketInstance.placeBet(7,{value: 7000000000000000000,from: user9}); 
-      const getbrttingpoint8 = await marketInstance.userBettingPoints(user9,7);
-      
+      const getbrttingpoint8 = await marketInstance.userBettingPoints(user9,7);   
       assert.equal(getbrttingpoint8/1,140);
-      })
+    })
 
-      it('user bet reward',async function() {
+    it('user bet reward',async function() {
+      // const checkBettingPoint = await marketInstance.userBettingPoints(user1,1);
+      // console.log(checkBettingPoint/1, 400);
       await marketInstance._closeBet(100);
-      // console.log(betclose.rewardToDistribute)
-      const reward = await marketInstance.WinningOption()
-      console.log(reward/1)
+      const checkBettingPoint1 = await marketInstance.userBettingPoints(user1,1);
+      const checkBettingPoint2 = await marketInstance.userBettingPoints(user5,1);
+      const checkBettingPoint3 = await marketInstance.userBettingPoints(user2,2);
+      const checkBettingPoint4 = await marketInstance.userBettingPoints(user3,3);
+      const checkBettingPoint5 = await marketInstance.userBettingPoints(user4,4);
+      const checkBettingPoint6 = await marketInstance.userBettingPoints(user6,2);
+      const checkBettingPoint7 = await marketInstance.userBettingPoints(user7,5);
+      const checkBettingPoint8 = await marketInstance.userBettingPoints(user8,6);
+      const checkBettingPoint9 = await marketInstance.userBettingPoints(user9,7);
+      // console.log("user1 betting points after close bet ",checkBettingPoint1/1);
+      // console.log("user2 betting points after close bet ",checkBettingPoint2/1);
+      // console.log("use3 betting points after close bet  ",checkBettingPoint3/1);
+      // console.log("user4 betting points after close bet ",checkBettingPoint4/1);
+      // console.log("user5 betting points after close bet ",checkBettingPoint5/1);
+      // console.log("user6 betting points after close bet ",checkBettingPoint6/1);
+      // console.log("user7 betting points after close bet ",checkBettingPoint7/1);
+      // console.log("user8 betting points after close bet ",checkBettingPoint8/1);
+      // console.log("user8 betting points after close bet ",checkBettingPoint9/1);
       await marketInstance.claimReward({from: user1});
-      console.log(claimReward);
-      const claim = await cliamaward.reward();
-      console.log(claim/1)
-      // const getbrttingpoint8 = await marketInstance.userBettingPoints(user9,7);
-      // console.log("user9",getbrttingpoint8/1)
-      // assert.equal(getbrttingpoint8/1,140);
-      // const reward = marketInstance.rewardToDistribute();
-      // console.log(reward/1)
+      const claim = await marketInstance.reward();
+      console.log("reward of user1",claim/1)
+      await marketInstance.claimReward({from: user5});
+      const claim2 = await marketInstance.reward();
+      console.log("reward of user5",claim2/1)
+      // const rewardTo = await marketInstance.rewardToDistribute()
+      // console.log("reward",rewardTo/1)
+      const optionPoint =  await marketInstance.optionsAvailable(1);
       })
      
      
