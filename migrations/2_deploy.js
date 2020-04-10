b42500528239f74afbd86a7b85054316c9d25ab0
udkreddySomish <dushyanth@somish.com>
2020-04-10 18:28:43 +0530

    Removed addNewMarket in migrations

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -16,7 +16,6 @@ module.exports = function(deployer, network, accounts){
       let deployMaster = await deployer.deploy(Master , deployPlotus.address, [deployMarketHourly.address, deployMarketDaily.address, deployMarketWeekly.address]);
       let plotusAddress = await deployMaster.plotusAddress();
       let plotus = await Plotus.at(plotusAddress);
-      await plotus.addNewMarket([Math.round(Date.now()/1000) + 100,1,2,1,7,1000000000000000,10000,2,2,100],"firstBet","0x47",["0xcd7e5d0cF2908850D185Ee9cc6377D6bb6DF0567","0xcd7e5d0cF2908850D185Ee9cc6377D6bb6DF0567"]);
   });
 };
 
