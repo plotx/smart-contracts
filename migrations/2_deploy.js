15598b6dbfe0a79ef68bb51e2cdafc8627d4139f
Parul2718 <67107635+Parul2718@users.noreply.github.com>
2020-07-02 15:06:20 +0530

    Update 2_deploy.js

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,23 +1,22 @@
 const Master = artifacts.require('Master');
 const Plotus = artifacts.require('Plotus');
-const MarketDaily = artifacts.require('MarketDailyMock');
-const MarketHourly = artifacts.require('MarketHourlyMock');
-const MarketWeekly = artifacts.require('MarketWeeklyMock');
+const MarketConfig = artifacts.require('MarketConfig');
+const Market = artifacts.require('Market');
 
 const BN = web3.utils.BN;
 
 module.exports = function(deployer, network, accounts){
   deployer.then(async () => {
       
-      let deployPlotus = await deployer.deploy(Plotus ,"0x39bE8E539972E15ff59A10c4ff1711b55164b49c");
-      let deployMarketHourly = await deployer.deploy(MarketHourly);
-      let deployMarketDaily = await deployer.deploy(MarketDaily);
-      let deployMarketWeekly = await deployer.deploy(MarketWeekly);
+      let deployPlotus = await deployer.deploy(Plotus);
+      let deployMarketHourly = await deployer.deploy(MarketConfig, [0,100,3,3600,1,1],[accounts[0],accounts[1]]);
+      let deployMarketDaily = await deployer.deploy(MarketConfig, [1,100,3,3600*24,1,1],[accounts[0],accounts[1]]);
+      let deployMarketWeekly = await deployer.deploy(MarketConfig, [2,100,3,3600*24*7,1,1],[accounts[0],accounts[1]]);
       let deployMaster = await deployer.deploy(Master , deployPlotus.address, [deployMarketHourly.address, deployMarketDaily.address, deployMarketWeekly.address]);
       let plotusAddress = await deployMaster.plotusAddress();
-      let plotus = await Plotus.at(plotusAddress);
+      // let plotus = await Plotus.at(plotusAddress);
       // let date = Math.round(Date.now()/1000) + 500;
-      // const addNewMarket = await plotus.addNewMarket([date,1,2,1,7,1000000000000000,10000,2,2,100],"firstBet","0x47",["0xcd7e5d0cF2908850D185Ee9cc6377D6bb6DF0567","0xcd7e5d0cF2908850D185Ee9cc6377D6bb6DF0567"])
+      // const addNewMarket = await plotus.addNewMarket(0,[date,100,5,3],"firstBet","0x47")
 
   });
 };
