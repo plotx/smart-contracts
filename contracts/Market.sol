7396f29bab0006e4ba0d4cc04e4df7dd46565fa7
nitika-goel <nitika.goel@somish.com>
2020-07-15 15:11:10 +0530

    Refactored logic

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -35,16 +35,19 @@ contract Market is usingOraclize {
     string internal FeedSource;//
     uint internal rate; //*
     // uint public currentPrice; //*
-    uint public currentPriceLocation;
-    uint public totalReward;//
+    uint public WinningOption;
+    // uint public totalReward;//
     bytes32 internal marketResultId;
     uint public rewardToDistribute;
     PredictionStatus internal predictionStatus;//
     uint public predictionForDate;//
     
-    mapping(address => mapping(uint=>uint)) internal ethStaked;
+    mapping(address => mapping(uint => uint)) internal ethStaked;
+    mapping(address => mapping(uint => uint)) internal LeverageEth;
     mapping(address => mapping(uint => uint)) internal userPredictionPoints;
     mapping(address => bool) internal userClaimedReward;
+    // option[] public options;
+    // predication[] predications;
     
     IPlotus internal pl;
     MarketConfig internal marketConfig;
@@ -55,18 +58,18 @@ contract Market is usingOraclize {
       uint maxValue;
       uint predictionPoints;
       uint ethStaked;
+      uint ethLeveraged;
     }
 
-  struct SentTx
-  {
-      address payable _userAdd;
-      uint _userStakedEth;
-      uint _userPredictionPoints;
-      uint _userLeverage;
-      uint _userDistance;
-      uint _userReturn;
-  }
-    mapping(uint=>SentTx[]) public _sentTx;
+  // struct predication
+  // {
+  //     address payable _userAdd;
+  //     uint _userStakedEth;
+  //     uint _userPredictionPoints;
+  //     uint _userLeverage;
+  //     uint _userReturn;
+  // }
+    // mapping(uint=>SentTx[]) public _sentTx;
     mapping(uint=>option) public optionsAvailable;
 
     modifier OnlyOwner() {
@@ -87,18 +90,19 @@ contract Market is usingOraclize {
       FeedSource = _feedsource;
       predictionForDate = _uintparams[1];
       rate = _uintparams[2];
-      optionsAvailable[0] = option(0,0,0,0);
+      // optionsAvailable[0] = option(0,0,0,0);
       (uint predictionTime, , , , , , ) = marketConfig.getPriceCalculationParams();
       expireTime = startTime + predictionTime;
       require(expireTime > now);
 
       // currentPrice = _uintparams[6];
-      setOptionRanges(_uintparams[3]);
-      setCurrentPrice(_uintparams[3]);
+      setOptionRanges(_uintparams[3],_uintparams[4]);
+      // setCurrentPrice(_uintparams[3]);
       // currentPriceLocation = _getDistance(OPTION_START_INDEX) + 1;
       // _setPrice();
       // closeMarketId = oraclize_query(expireTime.sub(now), "", "");
-      oraclize_query(expireTime.sub(now).add(predictionForDate), "URL", "json(https://financialmodelingprep.com/api/v3/majors-indexes/.DJI).price");
+      // Change to BTC oracle
+      oraclize_query(predictionForDate.sub(now), "URL", "json(https://financialmodelingprep.com/api/v3/majors-indexes/.DJI).price");
     }
 
     function () external payable {
@@ -112,13 +116,13 @@ contract Market is usingOraclize {
       return predictionStatus;
     }
 
-    //Need to add check Only Admin or Any authorized address
-    function setCurrentPrice(uint _currentPrice) public OnlyOwner {
-      require(now <= expireTime, "prediction closed");
-      // require(predictionStatus == PredictionStatus.Started,"bet closed");
-      // currentPrice = _currentPrice;
-      currentPriceLocation = _getCPDistanceFromFirstOption(_currentPrice);
-    }
+    // //Need to add check Only Admin or Any authorized address
+    // function setCurrentPrice(uint _currentPrice) public OnlyOwner {
+    //   require(now <= expireTime, "prediction closed");
+    //   // require(predictionStatus == PredictionStatus.Started,"bet closed");
+    //   // currentPrice = _currentPrice;
+    //   currentPriceLocation = _getCPDistanceFromFirstOption(_currentPrice);
+    // }
 
     function _calculateOptionPrice(uint _option, uint _totalStaked, uint _ethStakedOnOption, uint _totalOptions) internal view returns(uint _optionPrice) {
       _optionPrice = 0;
@@ -160,15 +164,14 @@ contract Market is usingOraclize {
       }
     }
 
-    function setOptionRanges(uint _currentPrice) internal{
-      (, , , , , , uint delta) = marketConfig.getPriceCalculationParams();
-        (, uint totalOptions, , , , ) = marketConfig.getBasicMarketDetails();
+    function setOptionRanges(uint _midRangeMin, uint _midRangeMax) internal{
+      
      optionsAvailable[1].minValue = 0;
-     optionsAvailable[1].maxValue = _currentPrice.sub(1);
-     optionsAvailable[2].minValue = _currentPrice;
-     optionsAvailable[2].maxValue = _currentPrice.add(200);
-     optionsAvailable[3].minValue = _currentPrice.add(1);
-     optionsAvailable[3].maxValue = ~uint256(0);
+     optionsAvailable[1].maxValue = _midRangeMin.sub(1);
+     optionsAvailable[2].minValue = _midRangeMin;
+     optionsAvailable[2].maxValue = _midRangeMax;
+     optionsAvailable[3].minValue = _midRangeMax.add(1);
+     optionsAvailable[3].maxValue = // max(uint256-1) ;
      
     }
 
@@ -226,47 +229,54 @@ contract Market is usingOraclize {
       require(now >= startTime && now <= expireTime);
       (, uint totalOptions, uint minPrediction, uint priceStep, , ) = marketConfig.getBasicMarketDetails();
       require(msg.value >= minPrediction,"Min prediction amount required");
-      uint _totalContribution = address(this).balance.sub(msg.value);
-      uint predictionValue = _calculatePredictionValue(_prediction, msg.value, _totalContribution, priceStep, totalOptions);
-      require(predictionValue > 0, "Stake too low");
-      userPredictionPoints[msg.sender][_prediction] = userPredictionPoints[msg.sender][_prediction].add(predictionValue);
+      // uint _totalContribution = address(this).balance.sub(msg.value);
+      // uint predictionValue = _calculatePredictionValue(_prediction, msg.value, _totalContribution, priceStep, totalOptions);
+      // require(predictionValue > 0, "Stake too low");
+      uint optionPrice = getOptionPrice();
+      uint predictionPoints = msg.value/optionPrice)*leverage;
+      //Add seafemath
+      userPredictionPoints[msg.sender][_prediction] = userPredictionPoints[msg.sender][_prediction].add(predictionPoints);
       ethStaked[msg.sender][_prediction] = ethStaked[msg.sender][_prediction].add(msg.value);
-      optionsAvailable[_prediction].predictionPoints = optionsAvailable[_prediction].predictionPoints.add(predictionValue);
+      LeverageEth[msg.sender][_prediction] = LeverageEth[msg.sender][_prediction].add(msg.value*leverage);
+      optionsAvailable[_prediction].predictionPoints = optionsAvailable[_prediction].predictionPoints.add(predictionPoints);
       optionsAvailable[_prediction].ethStaked = optionsAvailable[_prediction].ethStaked.add(msg.value);
-      _sentTx[_prediction].push(SentTx(msg.sender,msg.value,(predictionValue.mul(_leverage)), _leverage,0,0));
+       optionsAvailable[_prediction].ethLeveraged = optionsAvailable[_prediction].ethLeveraged.add(msg.value*leverage);
+      // _sentTx[_prediction].push(SentTx(msg.sender,msg.value,(predictionValue.mul(_leverage)), _leverage,0,0));
       
-      pl.callPlacePredictionEvent(msg.sender,msg.value, predictionValue, _prediction);
+      pl.callPlacePredictionEvent(msg.sender,msg.value, leverage, predictionPoints, _prediction);
     }
 
     function calculatePredictionResult(uint _value) public {
       require(msg.sender == pl.owner() || msg.sender == oraclize_cbAddress());
-      require(now >= expireTime.add(predictionForDate),"Time not reached");
+      require(now >= predictionForDate,"Time not reached");
 
       require(_value > 0);
       (, uint totalOptions, , , , uint bonuReturnPerc) = marketConfig.getBasicMarketDetails();
-      (, uint optionStartIndex, , , , , ) = marketConfig.getPriceCalculationParams();
+      // (, uint optionStartIndex, , , , , ) = marketConfig.getPriceCalculationParams();
+      //
 
+      uint totalReward = 0;
       predictionStatus = PredictionStatus.ResultDeclared;
-      for(uint i=optionStartIndex;i <= totalOptions;i++){
+      for(uint i=1;i <= totalOptions;i++){
         if(_value <= optionsAvailable[i].maxValue && _value >= optionsAvailable[i].minValue){
           // WinningOption = i;
           currentPriceLocation = i;
-        }         
-        else{
-         for(uint j = 0;j<_sentTx[i].length;j++){
-          if(currentPriceLocation > i){
-            _sentTx[i][j]._userDistance = currentPriceLocation.sub(i);
-          }   
-           else{
-            _sentTx[i][j]._userDistance = i.sub(currentPriceLocation); 
-           }    
-          uint totalLoss = (_sentTx[i][j]._userDistance.mul(10).mul(_sentTx[i][j]._userStakedEth).mul(_sentTx[i][j]._userLeverage)).div(100);
-          _sentTx[i][j]._userReturn = _sentTx[i][j]._userStakedEth.sub(totalLoss);
-          totalReward = totalReward.add(totalLoss);  
-          _transferEther(_sentTx[i][j]._userAdd, _sentTx[i][j]._userReturn); 
+
+
         }
         }
-      }
+        
+        for(uint i=1;i <= totalOptions;i++){
+
+          distanceFromWinningOption = ...//absolut value of i and currentPriceLocation
+          totalReward = totalReward + (distanceFromWinningOption * 10 * optionsAvailable[i].ethLeveraged/ 100 )
+        
+      
+        }
+
+       
+        
+      
       //Get donation, commission addresses and percentage
       
        (address payable donationAccount, uint donation, address payable commissionAccount, uint commission) = marketConfig.getFundDistributionParams();
@@ -280,38 +290,33 @@ contract Market is usingOraclize {
        pl.callMarketResultEvent(commission, donation);    
     }
 
-    function getReward(address _user)public view returns(uint){
-      uint userPoints;
-      for(uint i=0;i<_sentTx[currentPriceLocation].length;i++){
-        if(_sentTx[currentPriceLocation][i]._userAdd == msg.sender){
-         userPoints = userPoints.add(_sentTx[currentPriceLocation][i]._userPredictionPoints);
-        }
-      }
-      if(predictionStatus != PredictionStatus.ResultDeclared || userPoints == 0) {
+    function getReturn(address _user)public view returns(uint){
+     uint ethReturn = 0; 
+
+       if(predictionStatus != PredictionStatus.ResultDeclared ) {
         return 0;
       }
-      (uint reward) = _calculateReward(userPoints);
-      return reward;
+      for(uint i=0;i<optionAvailable.length;i++){
+        distancefromWinningoption//calc
+       ethReturn = ethReturn + ethStaked[_user][i] - ethLeveraged[_user][i] * distance * 10 / 100;
+      }
+      
+      uint reward = userPredictionPoints[_user][winningOption] * rewardToDistribute / optionsAvailable[currentPriceLocation].predictionPoints ;
+      return reward + ethReturn;
     }
 
-    function _calculateReward(uint userPoints) internal view returns(uint _reward) {
-      _reward = userPoints.mul(rewardToDistribute).div(optionsAvailable[currentPriceLocation].predictionPoints);
-    }
 
-    function claimReward() public {
+
+ 
+    function claimReturn() public {
       require(!userClaimedReward[msg.sender],"Already claimed");
       require(predictionStatus == PredictionStatus.ResultDeclared,"Result not declared");
       userClaimedReward[msg.sender] = true;
       uint userPoints;
-      for(uint i=0;i<_sentTx[currentPriceLocation].length;i++){
-        if(_sentTx[currentPriceLocation][i]._userAdd == msg.sender){
-         userPoints = userPoints.add(_sentTx[currentPriceLocation][i]._userPredictionPoints);
-        }
-      }
-      require(userPoints > 0,"must have atleast 0 points");
-      (uint reward) = _calculateReward(userPoints);
-      _transferEther(msg.sender, ethStaked[msg.sender][currentPriceLocation].add(reward));
-      pl.callClaimedEvent(msg.sender,reward, ethStaked[msg.sender][currentPriceLocation]);
+      
+      (uint return) = getReturn(msg.sender);
+       _recipient.transfer(_amount);
+      //pl.callClaimedEvent(msg.sender,reward, ethStaked[msg.sender][currentPriceLocation]);
     }
 
     function _transferEther(address payable _recipient, uint _amount) internal {
@@ -322,6 +327,10 @@ contract Market is usingOraclize {
       // if(myid == closeMarketId) {
       //   _closeBet();
       // } else if(myid == marketResultId) {
+
+        require ((myid==marketResultId));
+        //Check oraclise address
+        
         calculatePredictionResult(parseInt(result));
       // }
     }
