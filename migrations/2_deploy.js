fb8512efcb6a1c2c324829c697298a588ccd32ed
udkreddySomish <dushyanth@somish.com>
2020-08-05 17:58:48 +0530

    Updated migrations

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,5 +1,6 @@
 const Master = artifacts.require('Master');
 const Plotus = artifacts.require('Plotus');
+const PlotusToken = artifacts.require('PlotusToken');
 const MarketConfig = artifacts.require('MarketConfig');
 const Market = artifacts.require('Market');
 const MockchainLinkBTC = artifacts.require('MockChainLinkBTC');
@@ -9,9 +10,10 @@ module.exports = function(deployer, network, accounts){
   deployer.then(async () => {
       
       let deployPlotus = await deployer.deploy(Plotus);
+      let deployPlotusToken = await deployer.deploy(PlotusToken);
       let deployMockchainLinkBTC = await deployer.deploy(MockchainLinkBTC);
       let deployMarketHourly = await deployer.deploy(MarketConfig, [0,3,3600,2,2,'1000000000000000000'],[accounts[0],accounts[1],deployMockchainLinkBTC.address]);
-      let deployMaster = await deployer.deploy(Master , deployPlotus.address, [deployMarketHourly.address]);
+      let deployMaster = await deployer.deploy(Master , deployPlotus.address, [deployMarketHourly.address], deployPlotusToken.address);
       let plotusAddress = await deployMaster.plotusAddress();
       let plotus = await Plotus.at(plotusAddress);
   });
