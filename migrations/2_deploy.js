299ab32c62a296db554e0cec4c31cdac2a02dc9e
udkreddySomish <dushyanth@somish.com>
2020-08-05 20:46:11 +0530

    Updated migrations

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -10,10 +10,11 @@ module.exports = function(deployer, network, accounts){
   deployer.then(async () => {
       
       let deployPlotus = await deployer.deploy(Plotus);
+      let deployMarket = await deployer.deploy(Market);
       let deployPlotusToken = await deployer.deploy(PlotusToken);
       let deployMockchainLinkBTC = await deployer.deploy(MockchainLinkBTC);
       let deployMarketHourly = await deployer.deploy(MarketConfig, [0,3,3600,2,2,'1000000000000000000'],[accounts[0],accounts[1],deployMockchainLinkBTC.address]);
-      let deployMaster = await deployer.deploy(Master , deployPlotus.address, [deployMarketHourly.address], deployPlotusToken.address);
+      let deployMaster = await deployer.deploy(Master , deployPlotus.address, deployMarket.address, [deployMarketHourly.address], deployPlotusToken.address);
       let plotusAddress = await deployMaster.plotusAddress();
       let plotus = await Plotus.at(plotusAddress);
   });
