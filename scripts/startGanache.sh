f9ef6d241ec49cd9eec1a92f6c1ef86e80f0e02b
udkreddySomish <dushyanth@somish.com>
2020-08-10 09:58:27 +0530

    Added shortcuts for compiling and testing

---
+++ scripts/startGanache.sh
@@ -0,0 +1,6 @@
+@echo off
+echo Loading Resources . . .
+cd ../node_modules/.bin/
+echo starting ganache-cli  . . .
+./ganache-cli --gasLimit 0xfffffffffff -i 5777 -p 8545 -m 'grocery obvious wire insane limit weather parade parrot patrol stock blast ivory' -a 30 -e 10000000
+ping 127.0.0.1 -n 5 > nul
