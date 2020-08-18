c2a0d4b3bd436e7e6465fb7c12f45f3847c0c7e7
udkreddySomish <dushyanth@somish.com>
2020-08-19 03:15:54 +0530

    Resolved issues in governance and market

--- contracts/interfaces/IPlotus.sol
+++ contracts/interfaces/IPlotus.sol
@@ -42,7 +42,7 @@ contract IPlotus {
     * @param _prediction The option range on which user placed prediction.
     * @param _leverage The leverage selected by user at the time of place prediction.
     */
-    function callPlacePredictionEvent(address _user,uint _value, uint _predictionPoints, uint _predictionAsset, uint _prediction,uint _leverage) public{
+    function callPlacePredictionEvent(address _user,uint _value, uint _predictionPoints, address _predictionAsset, uint _prediction,uint _leverage) public{
     }
 
     /**
