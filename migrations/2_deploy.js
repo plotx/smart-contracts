cf13fcd1e298265cdb1db24bff79e5192393eeeb
udkreddySomish <dushyanth@somish.com>
2020-08-18 20:02:40 +0530

    Added BLOT in migrations

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -21,7 +21,7 @@ module.exports = function(deployer, network, accounts){
       let deployMemberRoles = await deployer.deploy(MemberRoles);
       let deployTokenController = await deployer.deploy(TokenController);
       let deployMarket = await deployer.deploy(Market);
-      let deployPlotusToken = await deployer.deploy(PlotusToken);
+      let deployPlotusToken = await deployer.deploy(PlotusToken, "30000000000000000000000000");
       let mockchainLinkBTC = await deployer.deploy(MockchainLinkBTC);
       let uniswapRouter = await deployer.deploy(MockUniswapRouter, deployPlotusToken.address);
       let marketConfig = await deployer.deploy(MarketConfig, [15*3600, '1000000000000000000',50,20,'100000000000000','100000000000000'],[accounts[0], mockchainLinkBTC.address, uniswapRouter.address, deployPlotusToken.address]);
@@ -32,6 +32,7 @@ module.exports = function(deployer, network, accounts){
 
       let plotusToken = await PlotusToken.at(deployPlotusToken.address);
       let tc = await TokenController.at(await master.getLatestAddress("0x5443"));
+      let blotToken = await deployer.deploy(BLOT, tc.address, plotusToken.address);
       await tc.changeDependentContractAddress();
       await plotusToken.changeOperator(await master.getLatestAddress("0x5443"));
       let plotusAddress = await master.getLatestAddress(web3.utils.toHex("PL"));
