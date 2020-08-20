421440dec15f780560aa8394d48b0615c4cc0470
udkreddySomish <dushyanth@somish.com>
2020-08-20 16:48:30 +0530

    Minor fix

--- test/blottoken.js
+++ test/blottoken.js
@@ -59,7 +59,7 @@ contract('bLOTToken', function([user1,user2]){
         await BLOTInstance.mint(user1,"1000000000000000000000");
         await BLOTInstance.approve(user1,"1000000000000000000")
         // let canTransferFrom = await BLOTInstance.transferFrom(user1,user2,"100000000000000000",{from : user1});
-        assert.ok(canTransferFrom)
+        // assert.ok(canTransferFrom)
         await assertRevert(BLOTInstance.transferFrom(user1,user2,"100000000000000000",{from : user2}))
         })
 
