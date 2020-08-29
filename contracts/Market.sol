ad0ee1ee6a8f9a24e456d7e2dfb701fef6424c07
udkreddySomish <dushyanth@somish.com>
2020-08-29 13:27:32 +0530

    Minor fix

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -419,7 +419,7 @@ contract Market is usingProvable {
       // _stakedBalance = _stakedBalance.sub(stakedTokenApplied[msg.sender]);
       // uint _stakedTokenRatio = _stakedBalance.div(_predictionValueInToken);
       // if(_stakedTokenRatio > _minMultiplierRatio) {
-      uint _muliplier = 100 + _stakedBalance.mul(100).div(_predictionValueInToken*mul(10));
+      uint _muliplier = 100 + _stakedBalance.mul(100).div(_predictionValueInToken.mul(10));
         // _stakedTokenRatio = _stakedTokenRatio.mul(10);
       predictionPoints = predictionPoints.mul(_muliplier).div(100);
       // }
