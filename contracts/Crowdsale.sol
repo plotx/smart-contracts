b01cc58c7ceb9ad314f9e2ac38fd574a8d28a58a
PremSOMISH <prem@somish.com>
2020-09-01 10:57:33 +0530

    Changes in comments

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -8,7 +8,7 @@ import "./external/openzeppelin-solidity/utils/ReentrancyGuard.sol";
 /**
  * @title Crowdsale
  * @dev Crowdsale is a base contract for managing a token crowdsale,
- * allowing investors to purchase tokens with ether. This contract implements
+ * allowing investors to purchase tokens with USDC/USDT. This contract implements
  * such functionality in its most fundamental form and can be extended to provide additional
  * functionality and/or custom behavior.
  * The external interface represents the basic interface for purchasing tokens, and conforms
