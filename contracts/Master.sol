277466bf2967572595f1a89aedbfdf411a6dabf9
Parv Garg <parv@somish.com>
2020-09-02 19:42:17 +0530

    linting improvements

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -8,7 +8,6 @@ import "./interfaces/ITokenController.sol";
 import "./interfaces/Iupgradable.sol";
 
 contract Master is Governed {
-
     bytes2[] public allContractNames;
     address public dAppToken;
     address public dAppLocker;
@@ -18,25 +17,35 @@ contract Master is Governed {
     mapping(bytes2 => address payable) public contractAddress;
 
     /**
-    * @dev modifier that allows only the authorized addresses to execute the function
-    */
+     * @dev modifier that allows only the authorized addresses to execute the function
+     */
     modifier onlyAuthorizedToGovern() {
         require(getLatestAddress("GV") == msg.sender, "Not authorized");
         _;
     }
 
     /**
-    * @dev Initialize the Master.
-    * @param _implementations The address of market implementation.
-    * @param _token The address of PLOT token.
-    * @param _marketUtiliy The addresses of market utility.
-    */
-    function initiateMaster(address[] calldata _implementations, address _marketImplementation, address _token, address _defaultbLOTMinter, address _marketUtiliy, address payable[] calldata _configParams, address _vesting) external {
-        OwnedUpgradeabilityProxy proxy =  OwnedUpgradeabilityProxy(address(uint160(address(this))));
+     * @dev Initialize the Master.
+     * @param _implementations The address of market implementation.
+     * @param _token The address of PLOT token.
+     * @param _marketUtiliy The addresses of market utility.
+     */
+    function initiateMaster(
+        address[] calldata _implementations,
+        address _marketImplementation,
+        address _token,
+        address _defaultbLOTMinter,
+        address _marketUtiliy,
+        address payable[] calldata _configParams,
+        address _vesting
+    ) external {
+        OwnedUpgradeabilityProxy proxy = OwnedUpgradeabilityProxy(
+            address(uint160(address(this)))
+        );
         require(!masterInitialised);
-        require(msg.sender == proxy.proxyOwner(),"Sender is not proxy owner.");
+        require(msg.sender == proxy.proxyOwner(), "Sender is not proxy owner.");
         masterInitialised = true;
-        
+
         //Initial contract names
         allContractNames.push("MR");
         allContractNames.push("PC");
@@ -45,44 +54,62 @@ contract Master is Governed {
         allContractNames.push("TC");
         allContractNames.push("BL");
 
-        require(allContractNames.length == _implementations.length);
+        require(
+            allContractNames.length == _implementations.length,
+            "Implementation length not match"
+        );
         contractsActive[address(this)] = true;
         dAppToken = _token;
         dAppLocker = _token;
-        for (uint i = 0; i < allContractNames.length; i++) {
+        for (uint256 i = 0; i < allContractNames.length; i++) {
             _generateProxy(allContractNames[i], _implementations[i]);
         }
 
         _setMasterAddress();
 
-        IPlotus(contractAddress["PL"]).initiatePlotus(_marketImplementation, _marketUtiliy, _token, _configParams);
+        IPlotus(contractAddress["PL"]).initiatePlotus(
+            _marketImplementation,
+            _marketUtiliy,
+            _token,
+            _configParams
+        );
         IbLOTToken(contractAddress["BL"]).initiatebLOT(_defaultbLOTMinter);
         ITokenController(contractAddress["TC"]).initiateVesting(_vesting);
     }
 
     /**
-    * @dev To check if we use the particular contract.
-    * @param _address The contract address to check if it is ative or not.
-    */
-    function isInternal(address _address) public view returns(bool) {
-      return contractsActive[_address];
+     * @dev To check if we use the particular contract.
+     * @param _address The contract address to check if it is active or not.
+     */
+    function isInternal(address _address) public view returns (bool) {
+        return contractsActive[_address];
     }
 
     /**
-    * @dev Gets latest contract address
-    * @param _contractName Contract name to fetch
-    */
-    function getLatestAddress(bytes2 _contractName) public view returns(address) {
+     * @dev Gets latest contract address
+     * @param _contractName Contract name to fetch
+     */
+    function getLatestAddress(bytes2 _contractName)
+        public
+        view
+        returns (address)
+    {
         return contractAddress[_contractName];
     }
 
     /**
-    * @dev adds a new contract type to master
-    */
-    function addNewContract(bytes2 _contractName, address _contractAddress) external onlyAuthorizedToGovern {
+     * @dev adds a new contract type to master
+     */
+    function addNewContract(bytes2 _contractName, address _contractAddress)
+        external
+        onlyAuthorizedToGovern
+    {
         require(_contractName != "MS", "Name cannot be master");
         require(_contractAddress != address(0), "Zero address");
-        require(contractAddress[_contractName] == address(0),"Contract code is already available.");
+        require(
+            contractAddress[_contractName] == address(0),
+            "Contract code already available"
+        );
         allContractNames.push(_contractName);
         _generateProxy(_contractName, _contractAddress);
         Iupgradable up = Iupgradable(contractAddress[_contractName]);
@@ -90,34 +117,37 @@ contract Master is Governed {
     }
 
     /**
-    * @dev upgrades a multiple contract implementations
-    */
-     function upgradeMultipleImplementations(
+     * @dev upgrades a multiple contract implementations
+     */
+    function upgradeMultipleImplementations(
         bytes2[] calldata _contractNames,
         address[] calldata _contractAddresses
-        ) 
-        external 
-        onlyAuthorizedToGovern 
-    {
-        require(_contractNames.length == _contractAddresses.length,"Array length should be equal.");
-        for (uint i=0; i < _contractNames.length; i++) {
-            require(_contractAddresses[i] != address(0),"null address is not allowed.");
+    ) external onlyAuthorizedToGovern {
+        require(
+            _contractNames.length == _contractAddresses.length,
+            "Array length should be equal."
+        );
+        for (uint256 i = 0; i < _contractNames.length; i++) {
+            require(
+                _contractAddresses[i] != address(0),
+                "null address is not allowed."
+            );
             _replaceImplementation(_contractNames[i], _contractAddresses[i]);
         }
     }
 
     /**
-    * @dev checks if an address is authorized to govern
-    */
-    function isAuthorizedToGovern(address _toCheck) public view returns(bool) {
+     * @dev checks if an address is authorized to govern
+     */
+    function isAuthorizedToGovern(address _toCheck) public view returns (bool) {
         return (getLatestAddress("GV") == _toCheck);
     }
 
     /**
-    * @dev Changes Master contract address
-    */
+     * @dev Changes Master contract address
+     */
     function _setMasterAddress() internal {
-        for (uint i = 0; i < allContractNames.length; i++) {
+        for (uint256 i = 0; i < allContractNames.length; i++) {
             Iupgradable up = Iupgradable(contractAddress[allContractNames[i]]);
             up.setMasterAddress();
         }
@@ -128,18 +158,26 @@ contract Master is Governed {
      * @param _contractsName The name of the contract.
      * @param _contractAddress The address of the contract to replace the implementations for.
      */
-    function _replaceImplementation(bytes2 _contractsName, address _contractAddress) internal {
-        OwnedUpgradeabilityProxy tempInstance 
-                = OwnedUpgradeabilityProxy(contractAddress[_contractsName]);
+    function _replaceImplementation(
+        bytes2 _contractsName,
+        address _contractAddress
+    ) internal {
+        OwnedUpgradeabilityProxy tempInstance = OwnedUpgradeabilityProxy(
+            contractAddress[_contractsName]
+        );
         tempInstance.upgradeTo(_contractAddress);
     }
 
     /**
-     * @dev to generater proxy 
+     * @dev to generator proxy
      * @param _contractAddress of the proxy
      */
-    function _generateProxy(bytes2 _contractName, address _contractAddress) internal {
-        OwnedUpgradeabilityProxy tempInstance = new OwnedUpgradeabilityProxy(_contractAddress);
+    function _generateProxy(bytes2 _contractName, address _contractAddress)
+        internal
+    {
+        OwnedUpgradeabilityProxy tempInstance = new OwnedUpgradeabilityProxy(
+            _contractAddress
+        );
         contractAddress[_contractName] = address(tempInstance);
         contractsActive[address(tempInstance)] = true;
     }
