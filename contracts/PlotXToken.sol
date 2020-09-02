c2d053cb5dbf1b3d2543457d5c59f694a6d602b5
Parv Garg <parv@somish.com>
2020-09-02 20:07:30 +0530

    linting improvements

--- contracts/PlotXToken.sol
+++ contracts/PlotXToken.sol
@@ -18,11 +18,10 @@ pragma solidity 0.5.7;
 import "./external/openzeppelin-solidity/token/ERC20/ERC20.sol";
 import "./external/openzeppelin-solidity/math/SafeMath.sol";
 
-
 contract PlotXToken is ERC20 {
     using SafeMath for uint256;
 
-    mapping(address => uint) public lockedForGV;
+    mapping(address => uint256) public lockedForGV;
 
     string public name = "PLOT";
     string public symbol = "PLOT";
@@ -30,7 +29,7 @@ contract PlotXToken is ERC20 {
     address public operator;
 
     modifier onlyOperator() {
-        require(msg.sender == operator);
+        require(msg.sender == operator, "Not operator");
         _;
     }
 
@@ -40,98 +39,108 @@ contract PlotXToken is ERC20 {
     }
 
     /**
-    * @dev change operator address 
-    * @param _newOperator address of new operator
-    */
-    function changeOperator(address _newOperator) public onlyOperator returns (bool) {
-        require(_newOperator != address(0));
+     * @dev change operator address
+     * @param _newOperator address of new operator
+     */
+    function changeOperator(address _newOperator)
+        public
+        onlyOperator
+        returns (bool)
+    {
+        require(_newOperator != address(0), "New operator cannot be 0 address");
         operator = _newOperator;
         return true;
     }
 
     /**
-    * @dev burns an amount of the tokens of the message sender
-    * account.
-    * @param amount The amount that will be burnt.
-    */
+     * @dev burns an amount of the tokens of the message sender
+     * account.
+     * @param amount The amount that will be burnt.
+     */
     function burn(uint256 amount) public returns (bool) {
         _burn(msg.sender, amount);
         return true;
     }
 
     /**
-    * @dev Burns a specific amount of tokens from the target address and decrements allowance
-    * @param from address The address which you want to send tokens from
-    * @param value uint256 The amount of token to be burned
-    */
+     * @dev Burns a specific amount of tokens from the target address and decrements allowance
+     * @param from address The address which you want to send tokens from
+     * @param value uint256 The amount of token to be burned
+     */
     function burnFrom(address from, uint256 value) public returns (bool) {
         _burnFrom(from, value);
         return true;
     }
 
     /**
-    * @dev function that mints an amount of the token and assigns it to
-    * an account.
-    * @param account The account that will receive the created tokens.
-    * @param amount The amount that will be created.
-    */
-    function mint(address account, uint256 amount) public onlyOperator returns(bool) {
+     * @dev function that mints an amount of the token and assigns it to
+     * an account.
+     * @param account The account that will receive the created tokens.
+     * @param amount The amount that will be created.
+     */
+    function mint(address account, uint256 amount)
+        public
+        onlyOperator
+        returns (bool)
+    {
         _mint(account, amount);
         return true;
     }
 
     /**
-    * @dev Transfer token for a specified address
-    * @param to The address to transfer to.
-    * @param value The amount to be transferred.
-    */
+     * @dev Transfer token for a specified address
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
     function transfer(address to, uint256 value) public returns (bool) {
-
-        require(lockedForGV[msg.sender] < now); // if not voted under governance
-        _transfer(msg.sender, to, value); 
+        require(lockedForGV[msg.sender] < now, "Locked for governance"); // if not voted under governance
+        _transfer(msg.sender, to, value);
         return true;
     }
 
     /**
-    * @dev Transfer tokens to the operator from the specified address
-    * @param from The address to transfer from.
-    * @param value The amount to be transferred.
-    */
-    function operatorTransfer(address from, uint256 value) public onlyOperator returns (bool) {
+     * @dev Transfer tokens to the operator from the specified address
+     * @param from The address to transfer from.
+     * @param value The amount to be transferred.
+     */
+    function operatorTransfer(address from, uint256 value)
+        public
+        onlyOperator
+        returns (bool)
+    {
         _transfer(from, operator, value);
         return true;
     }
 
     /**
-    * @dev Transfer tokens from one address to another
-    * @param from address The address which you want to send tokens from
-    * @param to address The address which you want to transfer to
-    * @param value uint256 the amount of tokens to be transferred
-    */
+     * @dev Transfer tokens from one address to another
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
     function transferFrom(
         address from,
         address to,
         uint256 value
-    )
-        public
-        returns (bool)
-    {
-        require(lockedForGV[from] < now); // if not voted under governance
+    ) public returns (bool) {
+        require(lockedForGV[from] < now, "Locked for governance"); // if not voted under governance
         _transferFrom(from, to, value);
         return true;
     }
 
     /**
-     * @dev Lock the user's tokens 
+     * @dev Lock the user's tokens
      * @param _of user's address.
      */
-    function lockForGovernanceVote(address _of, uint _period) public onlyOperator {
+    function lockForGovernanceVote(address _of, uint256 _period)
+        public
+        onlyOperator
+    {
         if (_period.add(now) > lockedForGV[_of])
             lockedForGV[_of] = _period.add(now);
     }
 
-    function isLockedForGV(address _of) public view returns(bool) {
+    function isLockedForGV(address _of) public view returns (bool) {
         return (lockedForGV[_of] > now);
     }
-
 }
