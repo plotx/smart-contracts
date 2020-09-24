5a91714880bf6375a083fcb262a74bc69462aec7
PremSOMISH <prem@somish.com>
2020-09-24 20:57:35 +0530

    Added contract and test cases for airdrop contract

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -15,12 +15,13 @@ contract Airdrop {
   uint public endDate;
 
   mapping(address => uint) public userAllocated;
+  mapping(address => bool) public userClaimed;
 
   constructor(address _plotToken, address _bLotToken, uint _endDate) public
   {
-    require(_plotToken != address(0));
-    require(_bLotToken != address(0));
-    require(_endDate > now);
+    require(_plotToken != address(0),"can not be null address");
+    require(_bLotToken != address(0),"can not be null address");
+    require(_endDate > now,"end date can not be past time");
     plotToken = PlotXToken(_plotToken);
     bLotToken = BLOT(_bLotToken);
     owner = msg.sender;
@@ -31,29 +32,27 @@ contract Airdrop {
   function airdropBLot(address[] calldata _userList, uint[] calldata _amount) external {
 
     require(msg.sender == owner,"Callable by owner only");
-    require(_userList.length == _amount.length);
+    require(_userList.length == _amount.length,"should have same length");
+    require(endDate > now, "Callable only before end date");
 
     for(uint i = 0; i < _userList.length; i++) {
       require(_userList[i] != address(0), "can not be null address");
-      require(userAllocated[_userList[i]] == 0,"Can allocate only once"); // need to rethink for condition, because if user claims thier tokens it will still be 0 and can allocate again.
+      require(_amount[i] > 0, "can not allocate 0");
+      require(userAllocated[_userList[i]] == 0,"Can allocate only once"); 
       userAllocated[_userList[i]] = _amount[i];
     }    
   }
 
   function takeLeftOverPlot() external {
-    require(endDate <= now, "Callable only after end date");
     require(msg.sender == owner,"Callable by owner only");
+    require(endDate <= now, "Callable only after end date");
     plotToken.transfer(owner, plotToken.balanceOf(address(this)));
   }
 
   function claim() external {
     require(endDate > now, "Callable only before end date");
-    require(userAllocated[msg.sender] > 0, "No Blot allocated");
-    uint _amount = userAllocated[msg.sender];
-    userAllocated[msg.sender] = 0;
-    bLotToken.mint(msg.sender, _amount);
-    
-  }
-
-  
+    require(!userClaimed[msg.sender], "Already claimed");
+    userClaimed[msg.sender] = true;
+    bLotToken.mint(msg.sender, userAllocated[msg.sender]);
+  } 
 }
