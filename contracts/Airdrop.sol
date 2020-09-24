cd2bbb5b4ca64e0c3256e1112dbecd68cd6a6156
PremSOMISH <prem@somish.com>
2020-09-24 13:07:13 +0530

    Made changes according to review

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -12,14 +12,20 @@ contract Airdrop {
   BLOT bLotToken;
   PlotXToken public plotToken;
   address owner;
+  uint public endDate;
 
-  constructor(address _plotToken, address _bLotToken) public
+  mapping(address => uint) public userAllocated;
+
+  constructor(address _plotToken, address _bLotToken, uint _endDate) public
   {
     require(_plotToken != address(0));
     require(_bLotToken != address(0));
+    require(_endDate > now);
     plotToken = PlotXToken(_plotToken);
     bLotToken = BLOT(_bLotToken);
     owner = msg.sender;
+    endDate = _endDate;
+    plotToken.approve(address(bLotToken), uint(1000000).mul(10 ** 18));
   }
 
   function airdropBLot(address[] calldata _userList, uint[] calldata _amount) external {
@@ -28,15 +34,25 @@ contract Airdrop {
     require(_userList.length == _amount.length);
 
     for(uint i = 0; i < _userList.length; i++) {
-      require(_userList[i] != address(0));
-      plotToken.approve(address(bLotToken), _amount[i]);
-      bLotToken.mint(_userList[i], _amount[i]);
+      require(_userList[i] != address(0), "can not be null address");
+      require(userAllocated[_userList[i]] == 0,"Can allocate only once"); // need to rethink for condition, because if user claims thier tokens it will still be 0 and can allocate again.
+      userAllocated[_userList[i]] = _amount[i];
     }    
   }
 
   function takeLeftOverPlot() external {
-    require (msg.sender == owner,"Callable by owner only");
+    require(endDate <= now, "Callable only after end date");
+    require(msg.sender == owner,"Callable by owner only");
     plotToken.transfer(owner, plotToken.balanceOf(address(this)));
+  }
+
+  function claim() external {
+    require(endDate > now, "Callable only before end date");
+    require(userAllocated[msg.sender] > 0, "No Blot allocated");
+    uint _amount = userAllocated[msg.sender];
+    userAllocated[msg.sender] = 0;
+    bLotToken.mint(msg.sender, _amount);
+    
   }
 
   
