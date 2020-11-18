fe66aea0a4234a8a7e2bf4016e9d72f9106914d7
PremSOMISH <prem@somish.com>
2020-11-18 20:38:43 +0530

    renamed variable names

--- test/utils/signAndExecuteMetaTx.js
+++ test/utils/signAndExecuteMetaTx.js
@@ -6,7 +6,7 @@ async function signAndExecuteMetaTx(...args) {
 	let pKey = args[0];
 	let types = args[1];
 	let values = args[2];
-	let user1 = args[3];
+	let user = args[3];
 	let functionSignature = args[4];
 	let contractInstance = args[5];
 
@@ -25,7 +25,7 @@ async function signAndExecuteMetaTx(...args) {
 		  sign1[1]= '0x' + ethutil.toUnsigned(ethutil.fromSigned(signature.r)).toString('hex');
 		  sign1[2]= '0x' + ethutil.toUnsigned(ethutil.fromSigned(signature.s)).toString('hex');
 
-		  await contractInstance.executeMetaTransaction(user1, functionSignature, sign1[1], sign1[2], sign1[0]);
+		  await contractInstance.executeMetaTransaction(user, functionSignature, sign1[1], sign1[2], sign1[0]);
 }
 
 module.exports = { signAndExecuteMetaTx };
