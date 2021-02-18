d9a689630db5fc16074d712b5d593db587132092
udkreddySomish <dushyanth@somish.com>
2021-02-18 22:57:58 +0530

    Updated ganache port

--- truffle-config.js
+++ truffle-config.js
@@ -44,7 +44,7 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
       gasPrice: 0,  // 20 gwei (in wei) (default: 100 gwei)
       gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
