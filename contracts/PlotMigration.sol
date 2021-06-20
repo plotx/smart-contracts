32bf430f8e830b8d0a9dfb61214fd35fac0a30c9
smrMadhan <madhan@somish.com>
2021-06-20 19:55:35 +0530

    Added withdraw and MultiSig to PlotMigration

--- contracts/PlotMigration.sol
+++ contracts/PlotMigration.sol
@@ -13,13 +13,16 @@
 pragma solidity 0.5.7;
 
 import "./interfaces/IToken.sol";
+import "./interfaces/IRootChainManager.sol";
 
 
 contract PLOTMigration {
  
-    address public migrationController; //0x3A6D2faBDf51Af157F3fC79bb50346a615c08BF6;
-    address public PLOTToken;
-    
+    address public MigrationController; //0x3A6D2faBDf51Af157F3fC79bb50346a615c08BF6; //multi sig
+    address public PLOTToken; //0x07ddc851a1bee757335ebcd7b14348359fdab60f;
+    address public PLOTMigrationL2; //0x32387Ae6c518a0efA2D8a3908d88CE4aFFCB0F01;
+    address public RootChainManager; //0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74
+    address public ERC20Predicate;//0xdD6596F2029e6233DEFfaCa316e6A95217d4Dc34
 
     /**
      * @dev Emitted when `value` tokens are moved from one account (`from`) to
@@ -29,22 +32,53 @@ contract PLOTMigration {
      */
     event Migrate(address indexed from, address indexed to, uint256 value);
     
-    constructor(address _PLOTToken,address _migrationController) public {
+    constructor(address _MigrationController,address _PLOTToken, address _plotMigrationL2, address _RootChainManager, address _ERC20Predicate) public {
         
-        migrationController = _migrationController;
+        MigrationController = _MigrationController;
         PLOTToken = _PLOTToken;
+        PLOTMigrationL2 = _plotMigrationL2;
+        RootChainManager = _RootChainManager;
+        ERC20Predicate = _ERC20Predicate;
+        initiateApproval();
     }
     
     /**
+     * @dev Approval to support DepositFor()
+     *
+     */
+    function initiateApproval() internal {
+        require(IToken(PLOTToken).approve(ERC20Predicate,115792089237316195423570985008687907853269984665640564039457584007913129639935));
+    }
+    
+    /**
+     * @dev Transfers Plot from user to the contract
+     * @param _to Address when the tokens to be received on Polygon
+     * @param _amount Value to be received in Ploygon
+     *
+     */
+    function migrate(address _to, uint256 _amount) public returns (bool){
+        require(IToken(PLOTToken).transferFrom(msg.sender, address(this), _amount));
+        emit Migrate(msg.sender,_to,_amount);
+        return true;
+    }
+    
+    /**
+     * @dev Transfers the tokens to Polygon migration contract
+     * @param _amount Value to be received in Ploygon
+     *
+     */
+    function depositFor(uint256 _amount) public {
+        require(msg.sender == MigrationController,"msg.sender should be Migration Controller");   
+        IRootChainManager(RootChainManager).depositFor(PLOTMigrationL2,PLOTToken,abi.encode(_amount));
+    }
+
+     /**
      * @dev Transfers Plot to Migration Controller
      *
      */
-    function migrate(
-        address _to,
-        uint256 _amount
-    ) public returns (bool){
-        require(IToken(PLOTToken).transferFrom(msg.sender, _to, _amount));
-        emit Migrate(msg.sender,_to,_amount);
+    function withdraw(uint256 _amount) public returns (bool){
+        require(msg.sender == MigrationController,"msg.sender should be Migration Controller");
+        require(IToken(PLOTToken).transfer(MigrationController, _amount));
         return true;
     }
    
